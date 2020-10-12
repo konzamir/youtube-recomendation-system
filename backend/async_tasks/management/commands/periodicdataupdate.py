ff53b8c6cd58bd797bb711cf5fd2955ea793308d
konzamir <konzamir@gmail.com>
2020-10-13 00:45:37 +0300

    finished periodic update task

--- backend/async_tasks/management/commands/periodicdataupdate.py
+++ backend/async_tasks/management/commands/periodicdataupdate.py
@@ -1,8 +1,24 @@
+import time
 from django.core.management.base import BaseCommand
+
+from videos.models import Video
+
+
+PACK_SIZE = 500
+SLEEP_TIME = 7 * 24 * 60 * 60  # 7 days for periodic update
 
 
 class Command(BaseCommand):
-    help = ''
+    help = 'Command for marking videos to update necessary data'
+
+    def _handle(self):
+        Video.objects.filter(
+            status=Video.VideoStatus.CHECKED
+        ).update(
+            status=Video.VideoStatus.NOT_CHECKED
+        )
 
     def handle(self, *args, **options):
-        pass
+        while True:
+            self._handle()
+            time.sleep(SLEEP_TIME)
